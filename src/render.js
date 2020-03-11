595179b2ceb50bebcc65357181daffc12988144a
King Cobra <kcobra557@gmail.com>
2020-03-11 00:38:46 +0000

    init render.js

--- src/render.js
+++ src/render.js
@@ -1,13 +1,28 @@
 // Modules
-const { desktopCapturer } = require('electron');
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
 
 // Buttons
 const videoElement = document.querySelector('video');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
 
 // Get the available video sources
 async function getVideoSources() {
-    
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+    });
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            };
+        })
+    );
+
+    videoOptionsMenu.popup();
 }
