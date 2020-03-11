d6a32e3c6dcf9e9f3b67da22e266bec593c717a7
King Cobra <kcobra557@gmail.com>
2020-03-11 03:03:28 +0000

    added recording and file saving capabilities

--- src/render.js
+++ src/render.js
@@ -1,11 +1,28 @@
 // Modules
 const { desktopCapturer, remote } = require('electron');
-const { Menu } = remote;
+const { writeFile } = require('fs');
+const { dialog, Menu } = remote;
+
+// Media Recorder instance to capture footage
+let mediaRecorder
+const recordedChunks = [];
 
 // Buttons
 const videoElement = document.querySelector('video');
 const startBtn = document.getElementById('startBtn');
+startBtn.onclick = e => {
+    mediaRecorder.start();
+    startBtn.classList.add('is-danger');
+    startBtn.innerText = 'Recording';
+  };
+
 const stopBtn = document.getElementById('stopBtn');
+stopBtn.onclick = e => {
+    mediaRecorder.stop();
+    startBtn.classList.remove('is-danger');
+    startBtn.innerText = 'Start';
+  };
+  
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 videoSelectBtn.onclick = getVideoSources;
 
@@ -47,4 +64,35 @@ async function selectSource(source) {
     // Preview the source in a video element
     videoElement.srcObject = stream;
     videoElement.play();
+
+    // Create the Media Recorder
+    const options = { mimeType: 'video/webm; codecs=vp9' };
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    // Register Event Handlers
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+}
+
+// Captures all recorded chunks
+function handleDataAvailable(e) {
+    console.log('Video Data Available')
+    recordedChunks.push(e.data);
+}
+
+// Saves the video file on stop
+async function handleStop(e) {
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm; codecs=vp9'
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+    const { filePath } = await dialog.showSaveDialog({
+        buttonLabel: 'save video',
+        defaultPath: `vid-${Date.now()}.webm`
+    });
+
+    if (filePath) {
+        writeFile(filePath, buffer, () => console.log('Video Saved successfully!'));
+    }
 }
