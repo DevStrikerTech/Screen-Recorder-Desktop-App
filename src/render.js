f638bbe4a3363c1ad4ac6049e53ffc57cc8903be
King Cobra <kcobra557@gmail.com>
2020-03-11 00:08:13 +0000

    adding buttons and video frames along some addition js module

---
+++ src/render.js
@@ -0,0 +1,13 @@
+// Modules
+const { desktopCapturer } = require('electron');
+
+// Buttons
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+
+// Get the available video sources
+async function getVideoSources() {
+    
+}
